9f172847ea18c2c2c7992e28ad8fb17490f5b9dd
Oliver Sewell <o.sewell@hotmail.com>
2016-08-20 16:21:56 +0100

    created a event handler when the user clicks on a blurry image the image will be replaced with the unblurry image

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hey beautiful");
+window.onload = init;
+
+function init() {
+  var images = document.getElementsByTagName("img");
+  for(var i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
+};
+
+function showAnswer(eventObj) {
+  var image = eventObj.target;
+  var name = image.id;
+  name = name + ".jpg";
+  image.src = name;
+};
